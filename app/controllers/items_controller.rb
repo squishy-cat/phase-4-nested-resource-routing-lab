2496a1f533fdaa3c7753544904ee360e5ff2cfb4
Eve Kozina <eve.kozina@gmail.com>
2022-01-12 14:52:54 -0500

    commit tree c98d74d2afae83c11bfe30fe2214bc80d4b24871

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,43 @@
 class ItemsController < ApplicationController
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find_by!(id: params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    if params[:user_id]
+      user = User.find_by(id: params[:user_id])
+      items = user.items.find_by!(id: params[:id])
+    else
+      items = Item.find_by(id: params[:id])
+    end
+    render json: items, include: :user
+  end
+
+  def create
+    if params[:user_id]
+      user = User.find_by(id: params[:user_id])
+      item = Item.create(item_params)
+      render json: item, status: :created
+    end
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
+  end
+
+  def render_not_found_response
+    render json: { error: "User not found" }, status: :not_found
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
 
+
+
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
   end
+
+  private
+
+
 
 end
